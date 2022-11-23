5f21f9d59d00bd53bdc6865e3270d1f32dc5c0ff
GsDrus39 <63257358+GsDrus39@users.noreply.github.com>
2022-11-23 16:24:42 +0200

    Update and rename pract2.1.cpp to pract2.cpp

--- pract2.cpp
+++ pract2.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 
-int main()
+int sum(std::string A, std::string B)
 {
-	std::string A = "999";
-	std::string B = "999";
+
 	int length;
 	bool flag = false;
 	int memor = 0;
 	std::string res;
 	if (A.length() < B.length())
 	{
-		length = A.length();
+		length = B.length();
 		flag = true;
 	}
 	else
 	{
-		length = B.length();
+		length = A.length();
 	}
-	
-	for (int i = 0; i < length; i++) 
+
+	for (int i = 0; i < std::abs(int(A.length() - B.length())); i++)
+	{
+		if (flag)
+		{
+			A = '0' + A;
+		}
+		else
+		{
+			B = '0' + B;
+		}
+	}
+
+	for (int i = 0; i < length; i++)
 	{
 		int lastfst = std::stoi(B.substr(length - 1 - i, 1));
 		int lastsnd = std::stoi(A.substr(length - 1 - i, 1));
-		if (flag) 
+		if (flag)
 		{
 			int lastfst = std::stoi(A.substr(length - 1 - i, 1));
 			int lastsnd = std::stoi(B.substr(length - 1 - i, 1));
@@ -37,6 +49,20 @@ int main()
 		B[length - 1 - i] = '\0';
 	}
 	res = std::to_string(memor) + res;
-	std::cout << std::stoi(res);
+	return std::stoi(res);
+}
+
+
+int main()
+{
+	std::string fst = "12";
+	std::string snd = "10";
+	
+	int res1 = 0;
+	for (int i = 0; i < std::stoi(snd); i++)
+	{
+		res1 = sum(std::to_string(res1), fst);
+	}
+	std::cout << res1;
 	return 0;
 }
